bf01d5d19a57ad106f9999b22c8b85d2c55e0044
Yuuponn <c0a2406889@edu.teu.ac.jp>
2025-06-17 16:30:22 +0900

    演習２をやりました

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,7 +8,11 @@ import time
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 DELTA: dict = {pg.K_UP:[0,-5],pg.K_DOWN:[0,5],pg.K_LEFT:[-5,0],pg.K_RIGHT:[5,0]}
+ALPHA: dict = {(-5,-5):[0.25,0.09]}
+KK_IMGS = {}
 
+def get_kk_img(sum_mv: tuple[int,int]):
+    return pg.transform.rotozoom(pg.image.load("fig/3.png"), ALPHA[sum_mv])
 
 def check_boud(rct: pg.Rect):
     """
@@ -30,6 +34,7 @@ def init_bb_imgs():
     for r in range(1,11):
         bb_img = pg.Surface((20*r,20*r))
         pg.draw.circle(bb_img,(255,0,0),(10*r,10*r),10*r)
+        bb_img.set_colorkey((0,0,0))
         bb_imgs.append(bb_img)
     
     return bb_imgs,bb_accs
@@ -53,6 +58,7 @@ def gameover(screen: pg.Surface):
     pg.display.update()
     time.sleep(5)
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -66,13 +72,15 @@ def main():
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0,WIDTH)
     bb_rct.centery = random.randint(0,HEIGHT)
-    bb_imgs, bb_speed = init_bb_imgs
     vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+    bb_imgs, bb_speed = init_bb_imgs()
 
     
     while True:
+        avx = bb_speed[min(tmr//500,9)]
+        bb_img = bb_imgs[min(tmr//500,9)]
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
@@ -84,27 +92,26 @@ def main():
         sum_mv = [0, 0]
         for key,mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] = mv[0]
-                sum_mv[1] = mv[1]
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
         if check_boud(kk_rct) != (True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
+        bb_rct.move_ip(vx*avx,vy*avx)
         yoko,tate = check_boud(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
         screen.blit(bb_img,bb_rct)
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
+        print(bb_img.get_rect())
         pg.display.update()
         tmr += 1
         clock.tick(50)
-        if tmr == 10:
-            vx = vx*bb_speed[min(tmr//500,9)]
-            bb_img = bb_imgs[min(tmr//500,9)]
-        init_bb_imgs
-
 
 if __name__ == "__main__":
     pg.init()
